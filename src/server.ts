38966c1802f43c8e06edc7db580aec6d8c0d6fb8
brunofsabino <brunoferrazsabino@gmail.com>
2022-06-16 16:57:31 -0300

    finalizando todo list

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
-import express from 'express'
+import express, {ErrorRequestHandler} from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import path from 'path'
 
 import router from './routes/index'
 
@@ -8,8 +9,18 @@ dotenv.config()
 
 const server = express()
 
+server.use(express.static(path.join(__dirname, '../public')))
+server.use(express.urlencoded({ extended: true }))
+
 server.use(cors())
 server.use(router)
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    res.status(400); // Bad Request
+    console.log(err);
+    res.json({ error: 'Ocorreu algum erro.' });
+}
+server.use(errorHandler);
 
 server.listen(process.env.PORT,()=>{
     console.log("Rodando na porta 3000")
